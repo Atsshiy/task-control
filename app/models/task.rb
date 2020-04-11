2ca02ce10ac0975821c6109fcd5b9740926ae51c
Attshiy <pakuchiy@gmail.com>
2020-04-11 11:47:44 +0000

    Finish C10

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
+  
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 200 }
+  
 end
